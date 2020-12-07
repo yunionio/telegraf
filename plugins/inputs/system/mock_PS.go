688722b803a55130d514ce6102fea0dce1e0c355
wanyaoqi <wanyaoqi@yunionyun.com>
2020-12-07 16:54:05 +0800

    feat(disk): disk plugin add ignore mount points
    In the container environment, some mount points is create
    by hypervisor, should be ignored.

--- plugins/inputs/system/mock_PS.go
+++ plugins/inputs/system/mock_PS.go
@@ -45,8 +45,10 @@ func (m *MockPS) CPUTimes(perCPU, totalCPU bool) ([]cpu.TimesStat, error) {
 	return r0, r1
 }
 
-func (m *MockPS) DiskUsage(mountPointFilter []string, fstypeExclude []string) ([]*disk.UsageStat, []*disk.PartitionStat, error) {
-	ret := m.Called(mountPointFilter, fstypeExclude)
+func (m *MockPS) DiskUsage(
+	mountPointFilter, ignoreMountPointFilter, fstypeExclude []string,
+) ([]*disk.UsageStat, []*disk.PartitionStat, error) {
+	ret := m.Called(mountPointFilter, ignoreMountPointFilter, fstypeExclude)
 
 	r0 := ret.Get(0).([]*disk.UsageStat)
 	r1 := ret.Get(1).([]*disk.PartitionStat)
