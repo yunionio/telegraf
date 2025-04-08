670a3d99339026f4fef54e733ffeb70f1272e853
Qu Xuan <qu_xuan@icloud.com>
2025-04-08 18:49:02 +0800

    fix: update post install script

--- scripts/rpm/post-install.sh
+++ scripts/rpm/post-install.sh
@@ -1,36 +1,8 @@
 #!/bin/bash
 
-BIN_DIR=/usr/bin
-LOG_DIR=/var/log/telegraf
-SCRIPT_DIR=/usr/lib/telegraf/scripts
-LOGROTATE_DIR=/etc/logrotate.d
-
-function install_init {
-    cp -f $SCRIPT_DIR/init.sh /etc/init.d/telegraf
-    chmod +x /etc/init.d/telegraf
-}
-
-function install_systemd {
-    cp -f $SCRIPT_DIR/telegraf.service $1
-    systemctl enable telegraf || true
-    systemctl daemon-reload || true
-}
-
-function install_update_rcd {
-    update-rc.d telegraf defaults
-}
-
-function install_chkconfig {
-    chkconfig --add telegraf
-}
-
 # Remove legacy symlink, if it exists
 if [[ -L /etc/init.d/telegraf ]]; then
     rm -f /etc/init.d/telegraf
-fi
-# Remove legacy symlink, if it exists
-if [[ -L /etc/systemd/system/telegraf.service ]]; then
-    rm -f /etc/systemd/system/telegraf.service
 fi
 
 # Add defaults file, if it doesn't exist
@@ -48,41 +20,31 @@ if [[ ! -f /etc/telegraf/telegraf.conf ]] && [[ -f /etc/telegraf/telegraf.conf.s
    cp /etc/telegraf/telegraf.conf.sample /etc/telegraf/telegraf.conf
 fi
 
-test -d $LOG_DIR || mkdir -p $LOG_DIR
+# Set up log directories
+LOG_DIR=/var/log/telegraf
+mkdir -p $LOG_DIR
 chown -R -L telegraf:telegraf $LOG_DIR
 chmod 755 $LOG_DIR
 
-# Distribution-specific logic
-if [[ -f /etc/redhat-release ]] || [[ -f /etc/SuSE-release ]]; then
-    # RHEL-variant logic
-    if [[ "$(readlink /proc/1/exe)" == */systemd ]]; then
-        install_systemd /usr/lib/systemd/system/telegraf.service
-    else
-        # Assuming SysVinit
-        install_init
-        # Run update-rc.d or fallback to chkconfig if not available
-        if which update-rc.d &>/dev/null; then
-            install_update_rcd
-        else
-            install_chkconfig
-        fi
-    fi
-elif [[ -f /etc/os-release ]]; then
-    source /etc/os-release
-    if [[ "$NAME" = "Amazon Linux" ]]; then
-        # Amazon Linux 2+ logic
-        install_systemd /usr/lib/systemd/system/telegraf.service
-    elif [[ "$NAME" = "Amazon Linux AMI" ]]; then
-        # Amazon Linux logic
-        install_init
-        # Run update-rc.d or fallback to chkconfig if not available
-        if which update-rc.d &>/dev/null; then
-            install_update_rcd
-        else
-            install_chkconfig
-        fi
-    elif [[ "$NAME" = "Solus" ]]; then
-        # Solus logic
-        install_systemd /usr/lib/systemd/system/telegraf.service
-    fi
+STATE_DIR=/var/lib/telegraf
+test -d "$STATE_DIR" || {
+    mkdir -p "$STATE_DIR"
+    chmod 770 "$STATE_DIR"
+    chown root:telegraf "$STATE_DIR"
+}
+
+STATE_FILE="$STATE_DIR/statefile"
+test -f "$STATE_FILE" || {
+    touch "$STATE_FILE"
+    echo {} > "$STATE_FILE"
+    chown root:telegraf "$STATE_FILE"
+    chmod 660 "$STATE_FILE"
+}
+
+# Set up systemd service - check if the systemd directory exists per:
+# https://www.freedesktop.org/software/systemd/man/sd_booted.html
+if [[ -d /run/systemd/system ]]; then
+    cp -f /usr/lib/telegraf/scripts/telegraf.service /usr/lib/systemd/system/telegraf.service
+    systemctl enable telegraf
+    systemctl daemon-reload
 fi
